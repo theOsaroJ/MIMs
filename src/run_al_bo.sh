b05c851ee5f38f6b29bfbd5dd7ebfb64b4eaf766
Osaro, 'Nosa James <64130121+theOsaroJ@users.noreply.github.com>
2025-05-17 11:33:40 -0700

    Update run_al_bo.sh

--- src/run_al_bo.sh
+++ src/run_al_bo.sh
@@ -3,4 +3,4 @@
 #$ -pe smp 16
 #$ -N AL_ei
 
-python3 al.py --acquisition ei --num_points_grid 500 1000 2000 5000 --query_size 1 > output.txt
+python3 al_bo.py --acquisition ei --num_points_grid 500 1000 2000 5000 --query_size 1 > output.txt
